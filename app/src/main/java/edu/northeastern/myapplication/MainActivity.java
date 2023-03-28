7a61a701e3c7119d12a686cfce0316824d58237e
mitchneides <mitch.neides@gmail.com>
2023-03-28 16:16:47 -0400

    Adds queue activity and button to main activity for access

--- app/src/main/java/edu/northeastern/myapplication/MainActivity.java
+++ app/src/main/java/edu/northeastern/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import edu.northeastern.myapplication.discoverpage.Discover;
+import edu.northeastern.myapplication.queue.MainActivityQueue;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void discover(View view) {
         Intent intent = new Intent(this, Discover.class);
+        startActivity(intent);
+    }
+
+    public void queue(View view) {
+        Intent intent = new Intent(this, MainActivityQueue.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/edu/northeastern/myapplication/queue/MainActivityQueue.java
@@ -0,0 +1,16 @@
+package edu.northeastern.myapplication.queue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.myapplication.R;
+
+public class MainActivityQueue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_queue);
+    }
+}
