c4aa18c11838719042a03baf2eaa47de6c79334b
mitchneides <mitch.neides@gmail.com>
2023-03-29 17:52:21 -0400

    User can join and leave queues. Now need to write algo to estimate time + add to db/calc from db

--- app/src/main/java/edu/northeastern/myapplication/queue/Queue_home.java
+++ app/src/main/java/edu/northeastern/myapplication/queue/Queue_home.java
@@ -4,10 +4,14 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import edu.northeastern.myapplication.R;
@@ -15,6 +19,10 @@ import edu.northeastern.myapplication.R;
 public class Queue_home extends AppCompatActivity {
     public static final int REQUEST_CODE = 1;
     private TextView in_queue;
+    private TextView est_wait;
+    private Button join_queue_btn;
+    private Button leave_queue_btn;
+    private boolean user_in_queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,12 @@ public class Queue_home extends AppCompatActivity {
         setContentView(R.layout.activity_queue_home);
 
         in_queue = findViewById(R.id.current_queue);
+        est_wait = findViewById(R.id.wait_est);
+        est_wait.setVisibility(View.INVISIBLE);
+        join_queue_btn = findViewById(R.id.join_queue_btn);
+        leave_queue_btn = findViewById(R.id.leave_queue_btn);
+        leave_queue_btn.setVisibility(View.INVISIBLE);
+        user_in_queue = false;
     }
 
     public void qr_scanner(View view) {
@@ -29,12 +43,66 @@ public class Queue_home extends AppCompatActivity {
         launcher.launch(intent);
     }
 
+    // receives the data from the QR Scanner
     private final ActivityResultLauncher<Intent> launcher =
             registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
                     result -> {
         if (result.getResultCode() == Activity.RESULT_OK) {
             String receivedData = result.getData().getStringExtra("data");
-            in_queue.setText("In Queue: " + receivedData);
+
+            // update text on screen and change queue buttons
+            swap_buttons(receivedData);
         }
     });
+
+    public void leave_queue(View view) {
+        // Warn user before leaving queue
+        AlertDialog alertDialog =
+                new AlertDialog.Builder(Queue_home.this).create();
+        alertDialog.setTitle("Are you sure?");
+        alertDialog.setMessage("You are about to leave the queue! If you confirm, you will lose" +
+                " your position in this queue.");
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Leave",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        swap_buttons("");
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+        alertDialog.show();
+    }
+
+    private void swap_buttons(String data) {
+        if (!user_in_queue) {
+            // update text on screen to queue joined
+            in_queue.setText("In Queue: " + data);
+            // change queue buttons to 'leave queue'
+            join_queue_btn.setVisibility(View.INVISIBLE);
+            leave_queue_btn.setVisibility(View.VISIBLE);
+            // calc and show estimated wait time
+            est_wait.setText(calc_wait_time());
+            est_wait.setVisibility(View.VISIBLE);
+            user_in_queue = true;
+        } else {
+            // update text on screen to default
+            in_queue.setText(getResources()
+                    .getString(R.string.use_the_button_below_to_join_a_queue));
+            // change queue buttons to 'join queue'
+            leave_queue_btn.setVisibility(View.INVISIBLE);
+            join_queue_btn.setVisibility(View.VISIBLE);
+            // hide est wait time
+            est_wait.setVisibility(View.INVISIBLE);
+            user_in_queue = false;
+        }
+    }
+
+    private String calc_wait_time() {
+        return "Position in Queue: ??\n\nEst. wait time: ??";
+    }
+
+
 }
