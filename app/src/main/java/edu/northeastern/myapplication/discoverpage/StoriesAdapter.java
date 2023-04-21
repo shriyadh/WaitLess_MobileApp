78858d2061e822da6201f7fb8d3dcb446224416b
shriyadh03 <shriyadh03@gmail.com>
2023-04-21 18:08:16 -0400

    fixed notify

--- app/src/main/java/edu/northeastern/myapplication/discoverpage/StoriesAdapter.java
+++ app/src/main/java/edu/northeastern/myapplication/discoverpage/StoriesAdapter.java
@@ -60,7 +60,7 @@ public class StoriesAdapter extends RecyclerView.Adapter<StoriesViewHolder> {
 
         if(position == 0){
             holder.user.setText(username);
-            holder.user.setTextColor(Color.WHITE);
+            holder.user.setTextColor(Color.GRAY);
             holder.user.setTypeface(holder.user.getTypeface(), Typeface.BOLD);
         }
         else if(username.length() > maxLen) {
